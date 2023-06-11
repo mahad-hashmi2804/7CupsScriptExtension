ad1b28e39cf663e90d3b17202f1a9c189b01847c
Mahad Hashmi <mahad.python@gmail.com>
2023-06-11 23:21:27 +0800

    Removed bootstrap stylesheet link because it was interfering with the page's font

--- source_code/contentScript.js
+++ source_code/contentScript.js
@@ -20,12 +20,12 @@ const offcanvas = document.createElement('div');
 // This is in such a horrible manner because I really don't know much JS, 
 // I just worked with what I did know.
 
-// So the first two lines here are bootstrap links
+// So the first line here is bootstrap JS for the offcanvas
 // After that comes the first div, meaning the actual offcanvas.
 offcanvas.innerHTML = `
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 
 <div class="offcanvas offcanvas-start" style="width: 50rem;" tabindex="-1" id="myOffcanvas" aria-labelledby="offcanvasExampleLabel">
 
